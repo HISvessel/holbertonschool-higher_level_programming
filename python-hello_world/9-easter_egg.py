a9a50764f2dfe54354e2fd606924996f25d3d291
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-07 16:43:59 -0400

    a test to our script

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters\n") 
+
+print("Beautiful is better than ugly.\n")
+print("Explicit is better than implicit.\n Simple is better than complex.\n")
+print("COmplex is better than complicated.\nFlat is better than nested.\nSParse is better than dense.\n")
