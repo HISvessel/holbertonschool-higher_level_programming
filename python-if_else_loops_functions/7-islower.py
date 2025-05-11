f6412f06bbaf1a08cb9b0c421a5f7b6b45da900a
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 12:27:15 -0400

    this function takes a char or index and determines if it is uppercase or lowercase

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(c):
+    letter = ord(c)
+    if letter in range(97, 123):
+        True
+        return letter
+    else:
+        pass
+
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("a is {}".format("lower" if islower("c") else "upper"))
+print("q is {}".format("lower" if islower("q") else "upper"))
