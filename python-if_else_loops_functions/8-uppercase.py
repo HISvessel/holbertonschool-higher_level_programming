80a4c37eb2f6de8489e5e9154fb3fd16e2e008b3
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 15:12:35 -0400

    this functino takes a string and converts the lowercase index into uppppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newstring = ""
+    for letter in str:
+        if ord(letter) in range(97, 123):
+            newstring += chr(ord(letter) - 32)
+        else:
+            newstring += letter
+    print(newstring)
