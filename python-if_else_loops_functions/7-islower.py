7d0c6115cd23c40f6d6cffffdf7de1d8069a2b55
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 12:38:41 -0400

    update:output fix and pycodestyle fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -5,8 +5,11 @@ def islower(c):
         True
         return letter
     else:
+        False
         pass
+
 
 print("3 is {}".format("lower" if islower("3") else "upper"))
 print("a is {}".format("lower" if islower("c") else "upper"))
 print("q is {}".format("lower" if islower("q") else "upper"))
+print("Y is {}".format("lower" if islower("Y") else "upper"))
