5eb44d323b3988dfa37709c2e98f71b0e28332a7
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 12:43:40 -0400

    update: testers removes, just sole function

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -7,9 +7,3 @@ def islower(c):
     else:
         False
         pass
-
-
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("a is {}".format("lower" if islower("c") else "upper"))
-print("q is {}".format("lower" if islower("q") else "upper"))
-print("Y is {}".format("lower" if islower("Y") else "upper"))
