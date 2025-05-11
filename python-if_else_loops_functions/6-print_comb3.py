57de0165c410719d1be17d9cad1b13eca6b27c6a
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 01:17:23 -0400

    update: pycodesyle fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range (10):
+for i in range(10):
     for j in range(10):
         if i == 8 and j == 9:
             print("{:d}{:d}". format(i, j), end="\n")
         else:
-            if j<= i:
+            if j <= i:
                 continue
             print("{:d}{:d}".format(i, j), end=", ")
