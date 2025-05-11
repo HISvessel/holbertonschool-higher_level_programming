4b67e38c5257cb9283c55785858fa15a2a5b6fb3
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 18:53:19 -0400

    printing an array of numbers, fizzbuzz on multiples of 15, fzz on multiples of 3 and buzz on multiples of 5

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(101):
+        if i == 100:
+            print("{:d}".format(i), end= "\n")
         if i % 15 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end= " ")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=" ")
         else:
             print("{:d}".format(i), end=" ")
