82f1d0975bc41a6dafdfda49932e41a0a7a388fa
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-11 01:14:41 -0400

    function that iterates through 99 and excludes all posseeeeeeeeeee repeats

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range (10):
     for j in range(10):
         if i == 8 and j == 9:
             print("{:d}{:d}". format(i, j), end="\n")
-        if j <= i:
-            continue
-        print("{:d}{:d}".format(i, j), end=", ")
-        
+        else:
+            if j<= i:
+                continue
+            print("{:d}{:d}".format(i, j), end=", ")
