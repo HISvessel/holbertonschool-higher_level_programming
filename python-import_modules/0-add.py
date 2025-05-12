967b2c00743c7d29a69b0a8d7947f70e9d047bdc
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-12 15:01:27 -0400

    update: codestyle fix and main fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__ main__":
+if __name__ == "__main__":
     a = int(1)
     b = int(2)
     print("{} + {} = {}".format(a, b, add(a, b)))
