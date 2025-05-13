bf1c7942b97ddd864ee85ad7496ef6571929a63e
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-12 22:31:54 -0400

    update: output and code requirement fx

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-counter = len(argv) - 1
-if counter == 0:
-    print("{} arguments".format(counter), end=".\n")
-elif counter == 1:
-    print("{} argument:\n{}: {}".format(counter, counter, argv[counter]))
-else:
-    print("{} arguments".format(counter), end=":\n")
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    counter = len(argv) - 1
+    if counter == 0:
+        print("{} arguments".format(counter), end=".\n")
+    elif counter == 1:
+        print("{} argument:\n{}: {}".format(counter, counter, argv[counter]))
+    else:
+        print("{} arguments".format(counter), end=":\n")
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
