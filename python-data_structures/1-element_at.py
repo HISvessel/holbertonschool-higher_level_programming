7fd0e58680adb7f942d62ea838364136f23101cd
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-14 13:12:19 -0400

    update: code output and return fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    i = my_list[idx]
-    return (i if idx in range(i) else None)
+    num = my_list[idx] if idx < len(my_list) else None
+    return num
