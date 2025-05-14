5c77eb370fa459cbd032e83ad56b46970aa8d679
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-14 12:38:26 -0400

    this is a test push to check for desired checker results

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in range(my_list):
-        idx = i
-        print("{:d}".format(idx))
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at {:d} is {}".format(idx, element_at(my_list, idx)))
+    i = my_list[idx]
+    return (i if idx in range(my_list) else None)
