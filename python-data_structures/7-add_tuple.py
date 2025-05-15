0f0ec99c3dbe7b1401fec19e8d5333215a873a3b
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-15 17:56:42 -0400

    update: output and case fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    combined = zip(tuple_a, tuple_b)
+    Tup_1 = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    Tup_2 = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    combined = zip(Tup_1, Tup_2)
     tuple_c = tuple(map(sum, combined))
     return tuple_c
