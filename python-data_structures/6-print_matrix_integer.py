6abfdec334808a947074da6d402f0e26245f4abe
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-15 13:45:37 -0400

    update: format stle prunting

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join(map(str, row)))
+        row = " ".join(map(str, row))
+        print("{:d}".format(row))
