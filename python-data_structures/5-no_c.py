a31aac5aedff809cfa702734fd9af060e3f8b9df
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-15 15:46:41 -0400

    update: output and case fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = "".join([chr for chr in my_string if chr.lower != 'c'])
+    new = "".join([chr for chr in my_string if chr != 'c' and chr != 'C'])
     return new
