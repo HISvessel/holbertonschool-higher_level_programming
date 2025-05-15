223ceab33630b45c7e62fc415835e17f6aa6506c
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-15 13:48:49 -0400

    update: output and format style fux

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        row = " ".join(map(str, row))
-        print("{}".format(row))
+        print("{:d} ".format(row))
