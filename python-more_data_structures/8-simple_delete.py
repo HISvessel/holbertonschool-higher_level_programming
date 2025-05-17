30860fd3e83cded2c8b951d5f694dbf91ac1224c
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-17 18:08:38 -0400

    update: output correct, now linter fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        a_dictionary.pop(key)   
+        a_dictionary.pop(key)
     else:
         del key
     return a_dictionary
