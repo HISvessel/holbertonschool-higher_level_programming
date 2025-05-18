1b0c9a29b9ce5c23bbbf626df0ff6642b5411977
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-18 17:12:31 -0400

    testing variable none  vs returning none

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         cuocient = a / b
     except ZeroDivisionError:
-        return None
+        cuocient = None
     finally:
         print("Inside result: {}".format(cuocient))
     return cuocient
