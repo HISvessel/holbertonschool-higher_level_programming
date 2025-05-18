905bc04e8613cfc1a91f869acd1dccc6ea68195b
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-18 17:05:15 -0400

    this function returns the division of two numbers except for a division by 0

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    cuocient = int
+    try:
+        cuocient = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(cuocient))
+    return cuocient
