0d5c81ead587b4788b2c6fcb316bbd4b6d0ab434
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-18 18:34:40 -0400

    this function returns a new list of the cuocient of the elements on a list with all its exceptions

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            cuocient = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+            cuocient = 0
+        except IndexError:
+            print("out of range")
+            cuocient = 0
+        finally:
+            new_list.append(cuocient)
+    return new_list
