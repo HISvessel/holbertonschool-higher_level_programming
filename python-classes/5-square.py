12c691e0dbff61b575ac9454e8b01b11735824fd
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-23 11:54:30 -0400

    this update prints a square to a given sixe

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""this is a new class with a few more modifications"""
+
+
+class Square:
+    """this new class has a few private instances
+    but also public instances of the class,
+      so that we may use and retrieve private data"""
+    def __init__(self, size=0):
+        """this function creates a private instance of a class"""
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """this function is created for the purpose of storing the
+        private size data for a public usage of that attribute"""
+        return self.__size
+
+    """this function uses the getter and setter method to
+    make an private instance be declared public"""
+    @size.setter
+    def size(self, value):
+        """raises errors if not the correct type
+        of if value is less than 0"""
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        return value
+
+    """this returns the area of a square's size"""
+    def area(self):
+        return self.__size ** 2
+
+    """this new function prints a set of
+    astersik up to the size of our square"""
+    def my_print(self):
+        """this should print out a new line
+        if size equals to 0, otherwise prints a square"""
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)
--- python-classes/tests.py
+++ python-classes/tests.py
@@ -3,9 +3,11 @@ class Person:
     """
      this starts a new class
      along with a greeting 
+    
 
      Update: greeting to the newly initialized user
     """
+    child = 2
     def introduction(self):
         print("Hola,", self.name,"tu tienes", self.age,"anos")
         print("Eres una", self.job)
@@ -19,12 +21,24 @@ class Person:
         self.age = age
         self.job = job
 
+class   Student:
+    school = "Holberton Coding School"
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
 
 first_child = Person("Emma", 2, "nina").introduction()
 second_child = Person("Luna", 1, "baby").introduction()
+print("We have a total of {} children".format(Person.child))
+"""^^^this utilizes the set instance of children inside of the class,"
+it cannot be changed outside of class, it is already set"""
 #introduction()
-print(first_child)
-print(second_child)
+#print(first_child)
+#print(second_child)
 """^^^^^^
 this ouputs None, for some reason 
 """
+Tommy = Student("Tommy", "A")
+#Access class attribute
+print(f"Tommy school: {Tommy.school}")
