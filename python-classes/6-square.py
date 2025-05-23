4e9d85ac0fc3c6dadcae20164dacd2d2328cb55d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-23 13:27:02 -0400

    testing a push

---
+++ python-classes/6-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""this is a new class with a few more modifications"""
+
+
+class Square:
+    """this new class has a few private instances
+    but also public instances of the class,
+      so that we may use and retrieve private data"""
+    def __init__(self, size=0, position=(0, 0)):
+        """this function creates a private instance of a class
+        a new instance is given by the name of position"""
+
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """this obtains the private instance called size"""
+        return self.__size
+    
+    """this new private instance position is now to be obtained"""
+    @property
+    def position(self):
+        """this returns the privae instance position"""
+        return self.__position
+    
+    """this function uses the getter and setter method to
+    make an private instance be declared public"""
+    @size.setter
+    def size(self, value):
+        """raises errors if not the correct type
+        of if value is less than 0"""
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        return value
+    
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if not isinstance(value, tuple()):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        return value
+
+
+    """this returns the area of a square's size"""
+    def area(self):
+        return self.__size ** 2
+
+    """this new function prints a set of
+    astersik up to the size of our square"""
+    def my_print(self):
+        """this should print out a new line
+        if size equals to 0, otherwise prints a square"""
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print("_" * self.position[0] + "#" * self.size)
+
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
