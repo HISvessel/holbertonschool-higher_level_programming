75e156b917700a2f37b52481f3b6901603b5af5e
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-25 18:12:04 -0400

    update: output fix

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,7 @@ from sys import argv
 from calculator_1 import add
 
 if __name__ == "__main__":
-    total = (add(total, int(arg)) for arg in argv[1:])
+    total = 0
+    for arg in argv[1:]:
+        total = add(total, int(arg))
     print("{}".format(total))
