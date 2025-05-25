4c90c4ab3f3937d85cc9baf7d6562fe7c1c25e15
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-25 19:25:44 -0400

    update: printing and returning

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return (print(abs(number) % 10, end=""))
+    num = abs(number) % 10
+    print(num, end="")
+    return num
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
