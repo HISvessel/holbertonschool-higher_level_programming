033c131104c3687361ade06f2bf637707afb45f7
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-26 22:32:12 -0400

    adding documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """creating a class that prints a sorted list"""
-
-
 class MyList(list):
+    """sorting a list when elements are appended
+    remains to be fixed"""
     def print_sorted(self):
         self.sort()
         print(self)
