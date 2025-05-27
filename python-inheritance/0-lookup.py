e65664d7dc36f1115968e6718ebb4b8e757ca930
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-26 21:13:22 -0400

    documentation fix

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""with this function, I can lookup the methods for the object that is input"""
+
+
 def lookup(obj):
+    """this function returns the methods contain in our objects"""
     return dir(obj)
