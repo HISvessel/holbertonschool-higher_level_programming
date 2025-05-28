e1ccbbe88c76029715734e179ca1ca8ff01b346d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-28 15:48:59 -0400

    doctesting and changing output

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,7 +5,8 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    """thi new subclass inherits from the basegeometry superclass"""
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, self.__width, self.__height)
+        super.integer_validator(self, self.__width, self.__height)
