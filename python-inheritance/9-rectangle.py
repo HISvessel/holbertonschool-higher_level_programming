a9b6c3bcae99c96d6ef928fad2630c821f7bebca
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-28 18:24:06 -0400

    updating the rectangle class to now return the area and a string lteral containing the class instances

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this module works with the inheritance of our previously created module"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """this new subclass inherits from the basegeometry superclass"""
+    def __init__(self, width, height):
+        """privatizing both of our instance attributes
+        inheriting the class function integer validator"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+    def area(self):
+        """updating the inherited area function
+        so that it now returns the area of the rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """returning a string containing the contents of the class"""
+        return (f"[Rectangle] {self.__width}/{self.__height}")
