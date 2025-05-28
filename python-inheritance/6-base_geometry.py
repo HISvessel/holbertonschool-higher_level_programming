976d29498dec05c5d3ed7a8ee99e2fbcdbd5f5a4
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-28 09:59:42 -0400

    a module with a non empty class that passes no inheritance

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module that creates a class of name BaseGeometry"""
+
+
+class BaseGeometry:
+    """this non empty class contains a public instance attribute"""
+    def area(self):
+        """passng a public class with an implementation exception"""
+        raise Exception("area() is not implemented")
