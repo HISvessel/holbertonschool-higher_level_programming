ccdb16de63d5e867e55d8c30709d2b4d16ec6fa6
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-28 15:47:59 -0400

    creating a new subclass

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module works with the inheritance of our previously created module"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, self.__width, self.__height)
