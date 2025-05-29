bfe97cbb0f2c1d0bce5f650e5fb37542822b0940
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-29 14:10:11 -0400

    validating integer twice

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -18,5 +18,6 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
         self.__str__()
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
+        self.integer_validator("size", size)
         self.area()
