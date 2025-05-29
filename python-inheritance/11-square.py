05613783a5cb6118991f258c3b1e08984e3451a0
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-29 15:01:45 -0400

    pdating the square class to contain the area and string methods

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this module contains a class that is created by
+inheriting attributes from other parent classes"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a new subclass called Square that inhertis from
+    rectangle, which itself inherits from BaseGeometry
+    To obtain attributes and rewrite them to make attributes with
+    custom properties, such as making 1 attribute of square(size)
+    perform as two atributes of rectangle(height, width)
+    we use super() method to override. The area method
+    is now implemented"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        self.__str__()
