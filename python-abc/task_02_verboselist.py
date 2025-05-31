3cd857a91319b107713fe2141c7baafcffeb160c
HISvessel <kevinsanchezdabest@yahoo.com>
2025-05-31 17:55:30 -0400

    this module modifies the behaviour of builtins and adds messages to confirm the function's performance and execution

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module contains a modified class of a list,
+which enables us to see the capacity to modify the behaviour
+of the list methods, as well as other methods"""
+
+
+class VerboseList(list):
+
+    """this class VerboseList inherits from the list object,
+    and this allows us to modify the hehavour and the messages
+    that are performed from the following methods
+    the append method, which prints that an item was added
+    the extend, which shows us the items that were extended
+    the remove method, which confirms that an item was deleted
+    the pop method, which pops the method at index. If an
+    index is not provided, we leave the function to perform its
+    default setting, which is to remove the last element of the list"""
+
+    def append(self, item):
+        print(f"Added [{item}] to the list.")
+        super().append(item)
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print(f"Extended the list with [{len(iterable)}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, item=-1):
+        print(f"Popped [{self[item]}] from the list")
+        super().pop(item)
+
+
+if __name__ == "__main__":
+    vl = VerboseList([1, 2, 3])
+    vl.append(4)
+    vl.extend([5, 6])
+    vl.remove(2)
+    vl.pop()
+    vl.pop(0)
+    print(vl)
