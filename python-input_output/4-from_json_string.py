3b1b54353cf34bea25451b3f6e9d197cd09787bd
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-03 17:36:31 -0400

    utilizing the loads function to pass a JSON obejct into a Python object. The conversion is performed by the very same function

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """this module contains a function that stores
-the data as given by the JSON interpretation of 
+the data as given by the JSON interpretation of
 a string and converts it into a Python object"""
 from json import loads
 
@@ -8,7 +8,7 @@ from json import loads
 def from_json_string(my_str):
     """a function that utilizes the loads builtin
     from the JSON module and converts it into a python
-    object. This can be used to better manipulate control 
+    object. This can be used to better manipulate control
     and output objects that will then be passed into JSON"""
 
     return loads(my_str)
