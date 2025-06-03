0d8e77a4eb54b139482f14b50226dff71846c7ea
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-03 11:12:08 -0400

    testing without the print directive

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
     not exist, it creates it by mean of the 'w' write argument"""
 
     with open(filename, "w") as f:
-        print(f.write(text), end="")
+        f.write(text)
