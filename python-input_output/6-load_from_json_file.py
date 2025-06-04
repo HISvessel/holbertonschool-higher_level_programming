b875716006e6406b2c2c7b52bf310527f9e02ce1
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-03 20:00:45 -0400

    this function loads from a json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this module contains the function load from json file
+which loads an object contained within a JSON file"""
+from json import loads
+
+
+def load_from_json_file(filename):
+    """the function is designed to read from a JSON
+    file and convert the returned string into an
+    object"""
+    
+    with open(filename) as f:
+        return loads(f.read())
