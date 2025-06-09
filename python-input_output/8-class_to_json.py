70d14b839a2f5260cdddbd8dc1768aaaf79250c5
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-08 20:36:49 -0400

    cpdestyle fix

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains the function called class to JSON"""
 
+
 def class_to_json(obj):
     """this function returns a string representation of a dictionary
     that can be serialized into a JSON string"""
