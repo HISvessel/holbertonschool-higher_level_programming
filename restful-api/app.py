4d06f99c7b320fd07d07545796cbcea2078c6536
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-16 23:43:27 -0400

    adding functionality for: adding books, updating books, deleting books and resetting book database file

--- restful-api/app.py
+++ restful-api/app.py
@@ -4,46 +4,102 @@ import os
 
 app = Flask(__name__)
 BOOK_FILE = "book_list.json"
+RESET_TOKEN = "my-secret-token"
 books = [] # an array of books to be uploaded
 @app.route('/') #the root route
 def home(): #function to display homescreen
     return "Welcome to the book API"
 
-@app.route('/books', methods=['GET']) # book redirection, the place where our primary data is stored
+@app.route("/books", methods=['GET']) # book redirection, the place where our primary data is stored
 def get_book():
-    return jsonify(books)
+    if os.path.exists(BOOK_FILE):
+        with open(BOOK_FILE) as file:
+            #data = json.load(file)
+            return jsonify(json.load(file))
+    return jsonify([]), 200
 
 @app.route("/books", methods=['POST'])
 def add_book(): # method to add a book to the display in our /book redirect
     new_book = request.get_json()
-    books = load_book_from_json()
+    books = load_from_json()
     books.append(new_book)
-    save_book_to_json(books)
+    save_to_json(books)
 
     return jsonify(new_book), 201
 
-@app.route('/books', methods=['GET'])
-def load_book_from_json():
+def load_from_json(): #loads book dictionary data contained in a JSON file
     if os.path.exists(BOOK_FILE):
         with open(BOOK_FILE) as file:
             return json.load(file)
-    return []
+    return [] #possible problem
 
-@app.route("/books/batch", methods=['POST'])
-def save_book_to_json(books):
+def save_to_json(books): # posts new data inside of a JSON file
     with open(BOOK_FILE, "w") as file:
         json.dump(books, file, indent=4)
     
-@app.route("", methods=['POST'])
-def batch_add_books():
-    many_books = request.get_json(books)
-    if many_books is not list:
-        return "400 Error (nad request)"
-    existing_books = load_book_from_json()
-    existing_books.extend(many_books)
-    save_book_to_json(existing_books)
+@app.route("/books/batch", methods=['POST'])
+def batch_add_books(): # method to add numerous books
+    many_books = request.get_json()
 
-    return jsonify(existing_books), 201
+    if not isinstance(many_books, list):
+        return jsonify({"Error": "bad request: expected a list of books"}), 404
+    try:
+        existing_books = load_from_json()
+        existing_books.extend(many_books)
+        save_to_json(existing_books)
+
+        return jsonify(existing_books), 201
+    except FileNotFoundError:
+        return jsonify({"Error": "File does not exist"}), 500
+
+@app.route("/books/<int:id>", methods= ['PUT'])
+def update_book(id): # method to update an existing book inside of our JSON file
+    updated_data = request.get_json()
+    try: 
+        books = load_from_json()
+        book_found = False
+        for book in books:
+            if book["id"] == id:
+                book.update(updated_data)
+                book_found = True
+                break
+
+        if book_found == False:
+            return jsonify({"Error": "book not found"}), 404
+        save_to_json(books)
+        return jsonify({"Success": "Book updated", "book": book}), 201
+    
+    except FileNotFoundError:
+            return jsonify({"Error": "File not found"}), 500
+        
+@app.route("/books/<int:id>", methods=["DELETE"])
+def delete_book(id):
+    try:
+        books = load_from_json()
+        book_found = False
+        for book in books:
+            if book["id"] == id:
+                books.remove(book)
+                book_found = True
+                break
+        if book_found == False:
+            return jsonify({"Error": "book not found"}), 404
+        
+        save_to_json(books)
+        return jsonify({"Message": "book deleted succesfully"}), 200
+
+    except FileNotFoundError:
+        return jsonify({"Error": "File not found"}), 500
+
+@app.route("/reset", methods= ["POST"])
+def reset_books():
+    if request.headers.get("X-Reset-Token") is not RESET_TOKEN:
+        return jsonify({"error": "unauthorized access"}), 403
+    seed_data = []
+    with open(BOOK_FILE, "w") as file:
+        json.dump(seed_data, file, indent=4)
+    return jsonify({"Message":"Books database reset successfully"}), 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
