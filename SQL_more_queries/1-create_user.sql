130dee2898d50b46a57737538d81d7495cb54e2d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-29 18:52:21 -0400

    removing user keyword for privileges propmt

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- this script creates a new user and grant all privileges to it
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRVILEGES ON *.* TO USER 'user_0d_1'@'localhost';
+GRANT ALL PRVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
