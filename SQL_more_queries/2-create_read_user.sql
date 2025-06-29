fd4a7a6c8ee837563805f9272dff6ef9f87325a9
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-29 19:35:26 -0400

    using the correct sql query command  to allow privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- privileges for said database.
 CREATE DATABASE IF NOT EXIST hbtn_0d_2;
 CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT READ PRVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
