68cb5e9b85b85bf233538553ed2a014956228329
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-29 13:36:54 -0400

    organizing all the relevant data

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- this script arranges the columns inside of a table in a descending order
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
