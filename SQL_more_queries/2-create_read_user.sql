130c803897169e7c11221109eb643cbaadccbaae
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-29 19:39:16 -0400

    syntax fix; calling the correct zexists keyword, not exist

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- this script creates a new database and a new user with read only 
 -- privileges for said database.
-CREATE DATABASE IF NOT EXIST hbtn_0d_2;
-CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
