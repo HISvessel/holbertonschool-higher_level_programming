4c31baaffa874694f2478989ad91e46c51a13838
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-29 22:59:35 -0400

    adding correct db statement

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3], charset="utf8")
         cursor = db.cursor()
-        command = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        command = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
         cursor.execute(command)
         rows = cursor.fetchall()
         for row in rows:
