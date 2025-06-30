61689dce047bb65f33a5f4e9927d71fbf5d17d56
HISvessel <kevinsanchezdabest@yahoo.com>
2025-06-29 20:05:26 -0400

    this script creates a new table with a non null default value

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- this script creates a table that holds a default not null value
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT = 1,
+    name VARCHAR(256)
+);
+SHOW CREATE TABLE id_not_null;
