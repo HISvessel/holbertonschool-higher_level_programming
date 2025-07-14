cc907a36c0b43709ffc6b69dca22914b99d91001
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-13 21:24:06 -0400

    semistandard fixes for clean code

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argument = process.argv.length -2
+const argument = process.argv.length - 2;
 
 if (argument === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argument === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
