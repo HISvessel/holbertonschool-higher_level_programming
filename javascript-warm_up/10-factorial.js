788a1b9ba2066190ddecbf124e599f12f12eeba8
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-18 16:32:00 -0400

    this script returns a factorial function of a command line input

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const index = parseInt(process.argv[2]);
+function factorial (n) {
+  if (n === 0 || n === 1 || isNaN(index)) {
+    return 1;
+  }
+  return n * factorial (n - 1);
+}
+
+console.log(factorial(index));
