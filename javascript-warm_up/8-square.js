fd0c2b10c168f7788cfaf2d700e15f0dbf26bb1e
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-18 01:30:19 -0400

    removing unnecessary semicolon

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const size = parseInt(process.argv[2]);
 
 if (isNaN(size)) {
   console.log("Missing size");
-};
+}
 
 for (let vertical = 0; vertical < size; vertical++) {
   console.log('X'.repeat(size));
