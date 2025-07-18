18a3947f002a4f6e25800af10c0ecaa8db456610
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-18 16:34:35 -0400

    fixed semistandard error

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@ function factorial (n) {
   if (n === 0 || n === 1 || isNaN(index)) {
     return 1;
   }
-  return n * factorial (n - 1);
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(index));
