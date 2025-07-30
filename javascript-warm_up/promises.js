1be651bbcc4407cac0aab461789b62ef481ba22d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-07-30 11:11:23 -0400

    updated changes to segment files

--- javascript-warm_up/promises.js
+++ javascript-warm_up/promises.js
@@ -58,12 +58,3 @@ async function init() {
 }
 
 init()
-
-/* Promise.all */
-const promise1 = Promise.resolve('Hello, mum');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-setTimeout(resolve, 2000, 'Bye'));
-
-Promise.all([promise1, promise2, promise3]).then(values => console.log(values))
-
